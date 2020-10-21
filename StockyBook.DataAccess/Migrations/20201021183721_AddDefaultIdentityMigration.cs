4623fd0c5880898e93efc6587107ca9b10ea46dd
anhnguyen <tintheanh@gmail.com>
2020-10-21 11:44:29 -0700

    finished setting up

---
+++ StockyBook.DataAccess/Migrations/20201021183721_AddDefaultIdentityMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockyBook.DataAccess.Migrations
+{
+    public partial class AddDefaultIdentityMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
